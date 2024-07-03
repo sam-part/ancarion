004f7088b78b7bb56c8f2c293c6f1e059437eb83
sam-part <124842992+sam-part@users.noreply.github.com>
2024-07-03 12:19:47 -0400

    Change character type

--- src/engine/types/glyph.h
+++ src/engine/types/glyph.h
@@ -3,13 +3,15 @@
 
 #include "color.h"
 
+using Character = char8_t;
+
 /*
  * Contains a character, foreground color, and background color
  * Represents a drawable ASCII character
  */
 struct Glyph
 {
- unsigned char character = ' ';
+ Character character = ' ';
 
  Color color    {255, 255, 255};
  Color bg_color {0, 0, 0};
